d75166cc459db816f125fe5fabcdbe9a00c70af5
ash0x <alexandergalev@Alexanders-MacBook.local>
2020-01-17 10:35:20 -0800

    first commit

---
+++ my.js
@@ -0,0 +1,9 @@
+"use strict"; // this code works the modern way
+
+[1, 2].forEach(alert);
+/* An example with two messages.
+This is a multiline comment.
+*/
+
+let message = 'Hello variable!'; // define the variable and assign the value
+alert(message);
